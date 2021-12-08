118d348176ffbbee22d254ba6fa40b2fa85a2b12
Amy Kirasack <amykirasack@gmail.com>
2021-12-08 17:12:51 -0500

    build: add contact form

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -10,6 +10,28 @@ const Contact = ({ id }) => {
       }}
     >
       <h1>📧 This is the Contact component 📧</h1>
+
+      <form name="contact" method="POST">
+        {/* data-netlify="true" <= add this as attribute to form tag */}
+        <p>
+          <label>
+            Name: <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Email: <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name="message"></textarea>
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
     </div>
   );
 };
